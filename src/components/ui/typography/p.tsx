b71352a39bdad914c1ba813b415820926fec113e
Jason Bura <jasoncbura@gmail.com>
2026-08-30 16:52:50 -0500

    feat: add render prop for asChild like functionality

--- src/components/ui/typography/p.tsx
+++ src/components/ui/typography/p.tsx
@@ -1,4 +1,6 @@
 import { cn } from '@/utils';
+import { mergeProps } from '@base-ui/react/merge-props';
+import { useRender } from '@base-ui/react/use-render';
 import { cva, VariantProps } from 'class-variance-authority';
 
 const pVariants = cva('', {
@@ -16,18 +18,22 @@ const pVariants = cva('', {
   },
 });
 
-function P({
-  className,
-  variant,
-  ...props
-}: React.ComponentProps<'p'> & VariantProps<typeof pVariants>) {
-  return (
-    <p
-      data-slot="p"
-      className={cn(pVariants({ variant, className }))}
-      {...props}
-    />
-  );
+interface PProps
+  extends useRender.ComponentProps<'p'>, VariantProps<typeof pVariants> {}
+
+function P({ render, className, variant, ...props }: PProps) {
+  const defaultProps: useRender.ElementProps<'p'> & {
+    'data-slot': string;
+  } = {
+    'data-slot': 'p',
+    className: cn(pVariants({ variant }), className),
+  };
+
+  return useRender({
+    defaultTagName: 'p',
+    render,
+    props: mergeProps<'p'>(defaultProps, props),
+  });
 }
 
 export { P, pVariants };
